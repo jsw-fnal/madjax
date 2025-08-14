438277a45098a75d10503eb54c479ea5511fab6c
Jon Wilson <Jon_Wilson2@baylor.edu>
2025-08-14 12:59:00 -0500

    Comment out option incompatible with jax 0.4

--- src/madjax/mg5/madjax_me_gen/EFT_madjax_reweight_implementation.py
+++ src/madjax/mg5/madjax_me_gen/EFT_madjax_reweight_implementation.py
@@ -31,7 +31,7 @@ jaxlogger.setLevel(logging.INFO)
 jax.config.update("jax_compilation_cache_dir", "jax_cache")
 jax.config.update("jax_persistent_cache_min_entry_size_bytes", -1)
 jax.config.update("jax_persistent_cache_min_compile_time_secs", 0)
-jax.config.update("jax_persistent_cache_enable_xla_caches", "xla_gpu_per_fusion_autotune_cache_dir")
+#jax.config.update("jax_persistent_cache_enable_xla_caches", "xla_gpu_per_fusion_autotune_cache_dir")
 
 class madjax_EFT:
     def __init__(self, madjax_instance_numerator, madjax_instance_denominator, WC_names=None):
