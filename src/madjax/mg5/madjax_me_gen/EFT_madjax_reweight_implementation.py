06666d39596c4823d92de6aac154fbf12a202d4c
Jon Wilson <jsw@fnal.gov>
2025-08-15 16:49:42 -0500

    Tone down the jax logging a bit, and turn on some jax local caching

--- src/madjax/mg5/madjax_me_gen/EFT_madjax_reweight_implementation.py
+++ src/madjax/mg5/madjax_me_gen/EFT_madjax_reweight_implementation.py
@@ -25,6 +25,13 @@ pjoin = os.path.join
 logger = logging.getLogger('decay.stdout') # -> stdout
 logger.setLevel(logging.INFO)
 
+jaxlogger = logging.getLogger("jax")
+jaxlogger.setLevel(logging.INFO)
+
+jax.config.update("jax_compilation_cache_dir", "jax_cache")
+jax.config.update("jax_persistent_cache_min_entry_size_bytes", -1)
+jax.config.update("jax_persistent_cache_min_compile_time_secs", 0)
+
 class madjax_EFT:
     def __init__(self, madjax_instance_numerator, madjax_instance_denominator, WC_names=None):
         self.numer = madjax_instance_numerator
