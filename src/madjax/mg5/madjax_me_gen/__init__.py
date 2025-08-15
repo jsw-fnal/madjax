3a191ae6007b9f3e6eaee646655959dbd74373ba
Jon Wilson <jsw@fnal.gov>
2025-08-15 16:38:18 -0500

    Made a double_reweight class that actually works

--- src/madjax/mg5/madjax_me_gen/__init__.py
+++ src/madjax/mg5/madjax_me_gen/__init__.py
@@ -35,7 +35,8 @@ new_cluster = {}
 new_interface = PluginInterface.MG5aMC_PythonMEsInterface
 
 # 4. Define a new reweight module
-new_reweight = {'EFT_madjax_rw': reweight_plugin.Double_reweight}
+#new_reweight = {'EFT_madjax_rw': reweight_plugin.Double_reweight}
+new_reweight = {'EFT_madjax_rw': reweight_plugin.EFT_madjax_reweight, 'Double': reweight_plugin.Double_reweight}
 
 
 ########################## CONTROL VARIABLE ####################################
